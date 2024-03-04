1ec67adcbcfb479e987fbc1be9745dfb1d6a8c54
Diederik de Vries <d.de.vries@hr.nl>
2024-03-04 12:25:26 +0100

    OPENAI-Key aangepast

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import os
 
 # Get the value of a specific environment variable
-variable_name = "USER"
+variable_name = "OPENAI_API_KEY"
 variable_value = os.getenv(variable_name)
 print(f"The value of {variable_name} is: {variable_value}")
-
-# Print all environment variables
-print("All environment variables:")
-for key, value in os.environ.items():
-    print(f"{key}: {value}")
